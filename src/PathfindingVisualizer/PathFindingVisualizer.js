abdf18c620ed51ea32b27856d4758199600af1d3
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-03-24 22:09:36 -0400

    node grid setup

--- src/PathfindingVisualizer/PathFindingVisualizer.js
+++ src/PathfindingVisualizer/PathFindingVisualizer.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Node from './Node/Node'
+
 
 export default class PathFindingVisualizer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nodes: []
+    }
+  }
+
+  componentDidMount(){
+    // when the component mounts loop through the grid and add the array into state
+    const nodes = []
+    for(let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for(let col = 0; col < 50; col++) {
+        currentRow.push([])
+      }
+      nodes.push(currentRow)
+    }
+    this.setState({nodes})
+  }
+
+
   render() {
     return (
-      <div>
-        
+      <div className='grid'>
+        {this.state.nodes.map((row, rowindex) => {
+          return <div>
+            {row.map((node, nodeIndex) => <Node></Node>)}
+            </div>
+        })}
       </div>
     )
   }
