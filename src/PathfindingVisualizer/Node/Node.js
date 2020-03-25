8a30ff303c240a5643d632a3213561a83f00fdb2
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-03-24 22:44:00 -0400

    fixing current node

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export default class Node extends Component {
   constructor(props) {
-  super(props)
-  this.state = {}
+    super(props)
+    this.state = {}
   }
 
   render() {
     return (
       <div className='node'>
-        
+
       </div>
     )
   }
--- src/PathfindingVisualizer/PathFindingVisualizer.js
+++ src/PathfindingVisualizer/PathFindingVisualizer.js
@@ -1,38 +1,52 @@
-import React, { Component } from 'react'
-import Node from './Node/Node'
-
+import React, { Component } from 'react';
+import Node from './Node/Node';
 
 export default class PathFindingVisualizer extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       nodes: []
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // when the component mounts loop through the grid and add the array into state
-    const nodes = []
-    for(let row = 0; row < 20; row++) {
+    const nodes = [];
+    for (let row = 0; row < 20; row++) {
       const currentRow = [];
-      for(let col = 0; col < 50; col++) {
-        currentRow.push([])
+      for (let col = 0; col < 50; col++) {
+        // currentRow.push([])
+        const currentNode = {
+          col,
+          row
+        };
+        currentRow.push(currentNode);
       }
-      nodes.push(currentRow)
+      nodes.push(currentRow);
     }
-    this.setState({nodes})
+    this.setState({ nodes });
   }
-
 
   render() {
     return (
       <div className='grid'>
-        {this.state.nodes.map((row, rowindex) => {
-          return <div>
-            {row.map((node, nodeIndex) => <Node></Node>)}
+        {this.state.nodes.map((row, rowIndex) => {
+          return (
+            <div key={rowIndex}>
+              {row.map((node, nodeIndex) => {
+                return (
+                  <Node
+                    key={nodeIndex}
+                    isStart={true}
+                    test={'as'}
+                    test={'asss'}>
+                    </Node>
+                );
+              })}
             </div>
+          );
         })}
       </div>
-    )
+    );
   }
 }
