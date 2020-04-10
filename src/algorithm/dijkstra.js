824e588a37d0cb562f6329eee5fb9fec507fd114
Sepehr Parirokh <sepehrparirokh@gmail.com>
2020-04-10 17:02:21 -0400

    visited path way

--- src/algorithm/dijkstra.js
+++ src/algorithm/dijkstra.js
@@ -32,11 +32,22 @@ function sortNodesByDistance(unvisitedNodes){
   unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
 
-function updateNeighbors(node, grid) {
-  const neighbors = getNeighbors(node, grid);
-  for(const neighbor of neighbors) {
-    neighbor,distance = node.distance +1;
+function updateUnvisitedNeighbors(node, grid) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for(const neighbor of unvisitedNeighbors) {
+    neighbor.distance = node.distance + 1;
+    neighbor.previousNode = node;
   }
+}
+
+function getUnvisitedNeighbors(node, grid) {
+  const neighbors = [];
+  const {col, row} = node;
+
+  if(row > 0) neighbors.push(grid[row-1][col]);
+  if(row < grid.length-1) neighbors.push(grid[row+1][col])
+  if(col > 0) neighbors.push(grid[row][col + 1]);
+  return neighbors.filter(neighbor => !neighbor.isVisited);
 }
 
 function getNeighbors(node, gird) {
